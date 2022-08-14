25e6a963b0f861a5061a086ae551e04a690f8b4c
Nischal Timalsina <94827555+nischaltimalsina@users.noreply.github.com>
2022-08-15 00:25:05 +0545

    Updates

--- src/hooks/useToggle.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-const useToggle = () => {
-  const [status, setStatus] = useState(false);
-  const toggle = () => setStatus((status) => !status);
-
-  return { status, setStatus, toggle };
-};
-export default useToggle;
